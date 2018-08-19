6a28ee0ed1209c93b0be637ca14c254ed72042db
Alisha Stilwell <alisha.stilwell@dudesolutions.com>
2018-08-19 19:25:56 -0400

    Initial commit

---
+++ HMCalendar/HMCalendar/HMCalendar/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HMCalendar.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
